e88a59dae8b0ccf99781cb52bb2f6acfd78c11e5
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-02-18 04:16:00 +0630

    Static Method

---
+++ OOP/03_Static_Method/src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+		Student.count = 10;
+		Student.getCount();
+		Student.count = 30;
+		Student.getCount();
+	}
+}
---
+++ OOP/03_Static_Method/src/Student.java
@@ -0,0 +1,12 @@
+public class Student
+{
+
+	// sttaic fields
+	public static int count;
+
+	// static methods
+	public static void getCount()
+	{
+		System.out.println("Count : " + count);
+	}
+}
