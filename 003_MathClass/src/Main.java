fd9a01ebb759769b0e3797c3e40ccac6583e0a4b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-02-17 11:30:13 +0630

    002_Math

---
+++ 003_MathClass/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/*
+Math -> min,max,ceil,floor,abs,pow,sqrt
+*/
+public class Main
+{
+	public static void main(String[] args)
+	{
+	int result = Math.max(1,10);
+	System.out.println(result);
+	}
+}
