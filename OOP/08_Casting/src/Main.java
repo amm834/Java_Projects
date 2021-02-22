38a332666b32019fdf337b26b9a8a1afd2677dc5
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-02-22 17:18:54 +0630

    08_Numeric Type Casting

---
+++ OOP/08_Casting/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		int x = 128;
+		byte y = (byte)x;
+		System.out.println(y);
+		
+		short z = (short)x;
+		System.out.println(z);
+		
+	}
+}
