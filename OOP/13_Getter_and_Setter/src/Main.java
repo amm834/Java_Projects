e6e3fcc5bc97f1a92cc6965e906bb4a27fc80f0f
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-03-19 22:59:13 +0630

    Getter and Setter

---
+++ OOP/13_Getter_and_Setter/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Student std1 = new Student();
+		std1.setId(10);
+		std1.setName("Aung Myat Moe");
+		System.out.println("ID : " + std1.getId()
+		+ " is " + std1.getName()
+		);
+	}
+}
