962408ff636d6ba4ccf56dc46db03c1382509bcb
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-03-25 18:32:51 +0630

    For Each

---
+++ 01 Core Java/004_For_Each/src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String [] names = {
+			"Mg Mg",
+			"Aung Aung",
+			"Mya Mya",
+			"Hla Hla"
+		};
+		for(String name : names){
+			System.out.println(name);
+		}
+
+	}
+}
