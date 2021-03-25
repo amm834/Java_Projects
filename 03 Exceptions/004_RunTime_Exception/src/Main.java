0bb8eeeec7818c4ef1feb0b2a7433c2ecb419f9b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-03-25 17:26:04 +0630

    Null Pointer Exception

---
+++ 03 Exceptions/004_RunTime_Exception/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main
+{
+	public static String name;
+	public static void main(String[] args)
+	{
+		int[] ary = {1,2,3,4,5};
+		try
+		{
+			System.out.println(ary[6]);
+		}
+		catch (ArrayIndexOutOfBoundsException e)
+		{
+			e.printStackTrace();
+		}
+		
+		name = "Aung Myat Moe";
+		System.out.println(name.length());
+		
+	}
+}
