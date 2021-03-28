bf75dd1d6208cac319847cfe5e7018ff6e721b00
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-03-28 20:16:40 +0630

    Wild Cards

---
+++ 04 IO/04_Wildcards/src/Box.java
@@ -0,0 +1,16 @@
+public class Box<T>
+	{
+		public T box;
+
+
+		public void setBox(T box)
+			{
+				this.box = box;
+			}
+
+		public T getBox()
+			{
+				return box;
+			}
+			
+	}
---
+++ 04 IO/04_Wildcards/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main
+	{
+		public static void main(String[] args)
+			{
+				Box<String> box = new Box<> ( );
+				box.setBox ( "Hello" );
+				
+				Box<Integer> box1 = new Box<>();
+				box1.setBox(123);
+				
+				Box<Person> box2 = new Box<>();
+				box2.setBox(new Person());
+				
+				Box<Student> box3 = new Box<>();
+				box3.setBox(new Student());
+				getBoxData ( box2);
+			}
+
+		private static void getBoxData(Box<? extends Person> box)
+			{
+				System.out.println ( box.getBox ( ) );
+			}
+	}
